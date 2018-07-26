fe2ed4cbc70b80c43699438802df24e1c5bf3250
jbaxe2 <jbaxe2@gmail.com>
2018-07-26 10:31:04 -0400

    Updated SDK version, dependencies, and code for Dart 2

--- lib/mongo_dart_query.dart
+++ lib/mongo_dart_query.dart
@@ -1,6 +1,6 @@
 library mongo_dart_query;
 
+import 'dart:convert' show json;
 import 'package:bson/bson.dart';
-import 'package:dart2_constant/convert.dart';
 part 'src/selector_builder.dart';
 part 'src/modifier_builder.dart';
